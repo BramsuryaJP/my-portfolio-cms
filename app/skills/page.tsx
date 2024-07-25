b4d50994f4f22514c41795e3ce4d842679c03254
BramsuryaJP <bramsuryajohannespaulus.work@gmail.com>
2024-07-25 22:26:37 +0700

    feat: change skill table header into dynamic

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,4 +1,7 @@
+import { SkillsTableHeader } from '@/lib/tableHeader'
+import React from 'react'
 import { BsPlus } from 'react-icons/bs'
+import { FaPencil, FaTrash } from 'react-icons/fa6'
 
 export default function Skills() {
 	return (
@@ -15,9 +18,9 @@ export default function Skills() {
 			</div>
 			<div className='mt-5'>
 				<div className='flex w-full items-center justify-end'>
-					<div className='bg-primaryLight dark:bg-primaryDark text-primaryDark dark:text-white border dark:border-primaryDarkBorder rounded-full p-1'>
+					<button className='bg-primaryLight dark:bg-primaryDark text-primaryDark dark:text-white border dark:border-primaryDarkBorder rounded-full p-1'>
 						<BsPlus size={28} />
-					</div>
+					</button>
 				</div>
 				<div className='mt-5 p-5 bg-primaryLight dark:bg-primaryDark border border-primaryLightBorder dark:border-primaryDarkBorder rounded-lg'>
 					<p className='text-lg font-bold text-primaryDark dark:text-white'>
@@ -28,209 +31,57 @@ export default function Skills() {
 							<table className='table'>
 								{/* head */}
 								<thead>
-									<tr>
+									<tr className='border-b-primaryLightBorder dark:border-b-primaryDarkBorder'>
 										<th>
 											<label>
 												<input
 													type='checkbox'
-													className='checkbox'
+													className='checkbox [--chkbg:theme(colors.secondary)] [--chkfg:theme(colors.primaryDark)] dark:[--chkfg:theme(colors.white)]'
 												/>
 											</label>
 										</th>
-										<th>Name</th>
-										<th>Job</th>
-										<th>Favorite Color</th>
-										<th></th>
+										{SkillsTableHeader.map(
+											(header, index) => (
+												<React.Fragment key={index}>
+													<th className='text-sm font-medium text-primaryDark dark:text-white/80'>
+														{header.name}
+													</th>
+												</React.Fragment>
+											)
+										)}
 									</tr>
 								</thead>
 								<tbody>
-									{/* row 1 */}
-									<tr>
+									<tr className='border-b-primaryLightBorder dark:border-b-primaryDarkBorder'>
 										<th>
 											<label>
 												<input
 													type='checkbox'
-													className='checkbox'
+													className='checkbox [--chkbg:theme(colors.secondary)] [--chkfg:theme(colors.primaryDark)] dark:[--chkfg:theme(colors.white)]'
 												/>
 											</label>
 										</th>
 										<td>
-											<div className='flex items-center gap-3'>
-												<div className='avatar'>
-													<div className='mask mask-squircle h-12 w-12'>
-														<img
-															src='https://img.daisyui.com/images/profile/demo/2@94.webp'
-															alt='Avatar Tailwind CSS Component'
-														/>
-													</div>
-												</div>
+											<div>
 												<div>
-													<div className='font-bold'>
+													<div className='text-sm font-bold text-primaryDark dark:text-white'>
 														Hart Hagerty
 													</div>
-													<div className='text-sm opacity-50'>
-														United States
-													</div>
 												</div>
 											</div>
 										</td>
-										<td>
-											Zemlak, Daniel and Leannon
-											<br />
-											<span className='badge badge-ghost badge-sm'>
-												Desktop Support Technician
-											</span>
-										</td>
-										<td>Purple</td>
 										<th>
-											<button className='btn btn-ghost btn-xs'>
-												details
-											</button>
-										</th>
-									</tr>
-									{/* row 2 */}
-									<tr>
-										<th>
-											<label>
-												<input
-													type='checkbox'
-													className='checkbox'
-												/>
-											</label>
-										</th>
-										<td>
-											<div className='flex items-center gap-3'>
-												<div className='avatar'>
-													<div className='mask mask-squircle h-12 w-12'>
-														<img
-															src='https://img.daisyui.com/images/profile/demo/3@94.webp'
-															alt='Avatar Tailwind CSS Component'
-														/>
-													</div>
-												</div>
-												<div>
-													<div className='font-bold'>
-														Brice Swyre
-													</div>
-													<div className='text-sm opacity-50'>
-														China
-													</div>
-												</div>
+											<div className='flex flex-row gap-5 text-sm font-bold text-primaryDark dark:text-white'>
+												<button>
+													<FaPencil />
+												</button>
+												<button>
+													<FaTrash />
+												</button>
 											</div>
-										</td>
-										<td>
-											Carroll Group
-											<br />
-											<span className='badge badge-ghost badge-sm'>
-												Tax Accountant
-											</span>
-										</td>
-										<td>Red</td>
-										<th>
-											<button className='btn btn-ghost btn-xs'>
-												details
-											</button>
-										</th>
-									</tr>
-									{/* row 3 */}
-									<tr>
-										<th>
-											<label>
-												<input
-													type='checkbox'
-													className='checkbox'
-												/>
-											</label>
-										</th>
-										<td>
-											<div className='flex items-center gap-3'>
-												<div className='avatar'>
-													<div className='mask mask-squircle h-12 w-12'>
-														<img
-															src='https://img.daisyui.com/images/profile/demo/4@94.webp'
-															alt='Avatar Tailwind CSS Component'
-														/>
-													</div>
-												</div>
-												<div>
-													<div className='font-bold'>
-														Marjy Ferencz
-													</div>
-													<div className='text-sm opacity-50'>
-														Russia
-													</div>
-												</div>
-											</div>
-										</td>
-										<td>
-											Rowe-Schoen
-											<br />
-											<span className='badge badge-ghost badge-sm'>
-												Office Assistant I
-											</span>
-										</td>
-										<td>Crimson</td>
-										<th>
-											<button className='btn btn-ghost btn-xs'>
-												details
-											</button>
-										</th>
-									</tr>
-									{/* row 4 */}
-									<tr>
-										<th>
-											<label>
-												<input
-													type='checkbox'
-													className='checkbox'
-												/>
-											</label>
-										</th>
-										<td>
-											<div className='flex items-center gap-3'>
-												<div className='avatar'>
-													<div className='mask mask-squircle h-12 w-12'>
-														<img
-															src='https://img.daisyui.com/images/profile/demo/5@94.webp'
-															alt='Avatar Tailwind CSS Component'
-														/>
-													</div>
-												</div>
-												<div>
-													<div className='font-bold'>
-														Yancy Tear
-													</div>
-													<div className='text-sm opacity-50'>
-														Brazil
-													</div>
-												</div>
-											</div>
-										</td>
-										<td>
-											Wyman-Ledner
-											<br />
-											<span className='badge badge-ghost badge-sm'>
-												Community Outreach Specialist
-											</span>
-										</td>
-										<td>Indigo</td>
-										<th>
-											<button className='btn btn-ghost btn-xs'>
-												details
-											</button>
 										</th>
 									</tr>
 								</tbody>
-								{/* foot */}
-								<tfoot>
-									<tr>
-										<th></th>
-										<th>Name</th>
-										<th>Job</th>
-										<th>Favorite Color</th>
-										<th></th>
-									</tr>
-								</tfoot>
 							</table>
 						</div>
 					</div>
---
+++ lib/tableHeader.ts
@@ -0,0 +1,1 @@
+export const SkillsTableHeader = [{ name: 'name' }, { name: 'action' }] as const
