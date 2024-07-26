5f90b37dc9c8629a44b126c4332871eace7dc3c3
BramsuryaJP <bramsuryajohannespaulus.work@gmail.com>
2024-07-26 13:39:10 +0700

    feat: add profile dropdown component

--- components/navigation/Sidebar.tsx
+++ components/navigation/Sidebar.tsx
@@ -9,6 +9,7 @@ import ThemeToggler from '../ThemeToggler'
 import { FaBars, FaCaretLeft } from 'react-icons/fa6'
 import { FaTimes } from 'react-icons/fa'
 import { BiLogOut } from 'react-icons/bi'
+import { ProfileDropdown } from '../auth/ProfileDropdown'
 
 export const Sidebar = ({ children }: SidebarProps) => {
 	const pathname = usePathname()
@@ -32,57 +33,12 @@ export const Sidebar = ({ children }: SidebarProps) => {
 						Portfolio CMS
 					</div>
 					<div className='flex lg:hidden'>
-						<Menu>
-							<MenuButton
-								as='button'
-								className='w-10 h-10 rounded-full bg-primaryLight/80 dark:bg-primaryDarkBg/80 backdrop-blur-xl border border-primaryLightBorder dark:border-primaryDarkBorder flex items-center justify-center'
-							>
-								<p className='uppercase text-lg font-semibold text-primaryDark dark:text-white'>
-									N
-								</p>
-							</MenuButton>
-							<MenuItems
-								anchor='bottom end'
-								className='mt-2 rounded-md dark:bg-primaryDarkBg/50 backdrop-blur-xl border border-primaryLightBorder dark:border-primaryDarkBorder'
-							>
-								<MenuItem>
-									<ThemeToggler />
-								</MenuItem>
-								<div className='my-1 h-px bg-white/5' />
-							</MenuItems>
-						</Menu>
+						<ProfileDropdown />
 					</div>
 					<div className='hidden flex-none lg:block'>
 						<ul className='menu menu-horizontal'>
 							{/* Navbar menu content here */}
-							<Menu>
-								<MenuButton
-									as='button'
-									className='w-10 h-10 rounded-full bg-primaryLight/80 dark:bg-primaryDarkBg/80 backdrop-blur-xl border border-primaryLightBorder dark:border-primaryDarkBorder flex items-center justify-center'
-								>
-									<p className='uppercase text-lg font-semibold text-primaryDark dark:text-white'>
-										N
-									</p>
-								</MenuButton>
-								<MenuItems
-									anchor='bottom end'
-									className='mt-2 rounded-md dark:bg-primaryDarkBg/50 backdrop-blur-xl border border-primaryLightBorder dark:border-primaryDarkBorder'
-								>
-									<MenuItem>
-										<ThemeToggler />
-									</MenuItem>
-									<div className='my-1 h-px bg-primaryLightBorder dark:bg-primaryDarkBorder' />
-									<MenuItem
-										as='button'
-										className='flex flex-row items-center gap-2 p-4'
-									>
-										<BiLogOut color='#C63C51' size={20} />
-										<p className='text-sm font-semibold text-[#C63C51]'>
-											Logout
-										</p>
-									</MenuItem>
-								</MenuItems>
-							</Menu>
+							<ProfileDropdown />
 						</ul>
 					</div>
 				</div>
@@ -115,7 +71,20 @@ export const Sidebar = ({ children }: SidebarProps) => {
 
 					<div className='flex flex-col gap-2 py-4 ps-4'>
 						{links.map((link, index) => (
-							<Link href={link.url} key={index} className='flex'>
+							<Link
+								href={link.url}
+								key={index}
+								className='flex'
+								onClick={() => {
+									const drawerCheckbox =
+										document.getElementById(
+											'my-drawer-3'
+										) as HTMLInputElement | null
+									if (drawerCheckbox) {
+										drawerCheckbox.checked = false
+									}
+								}}
+							>
 								<li className='py-1 flex-1'>
 									<p className='text-base text-primaryDark dark:text-white hover:bg-transparent focus:!bg-transparent active:!bg-transparent focus:!text-primaryDark active:!text-primaryDark dark:focus:!text-white dark:active:!text-white'>
 										{link.name}
